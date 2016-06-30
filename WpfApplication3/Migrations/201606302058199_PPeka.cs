da72096ad17c832efab4d8094e38f60e2a947981
estevaogut <estevaorg@gmail.com>
2016-06-30 18:16:31 -0300

    Bibliopeka v1.2

---
+++ WpfApplication3/Migrations/201606302058199_PPeka.cs
@@ -0,0 +1,39 @@
+namespace WpfApplication3.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PPeka : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Livros",
+                c => new
+                    {
+                        IdLivro = c.Int(nullable: false, identity: true),
+                        Nome = c.String(),
+                        Coletanea = c.String(),
+                        Ano = c.String(),
+                    })
+                .PrimaryKey(t => t.IdLivro);
+            
+            CreateTable(
+                "dbo.Pessoas",
+                c => new
+                    {
+                        IdPessoa = c.Int(nullable: false, identity: true),
+                        Nome = c.String(),
+                        CPF = c.String(),
+                    })
+                .PrimaryKey(t => t.IdPessoa);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Pessoas");
+            DropTable("dbo.Livros");
+        }
+    }
+}
